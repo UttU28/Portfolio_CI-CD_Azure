88f4fb821f99e5e7e536635a1852d33e79a53459
UttU28 <utsavmaan28@gmail.com>
2024-04-11 02:52:32 -0500

    Abhi Chalega na Bidhu

--- uploadFiles.tf
+++ uploadFiles.tf
@@ -1,60 +1,60 @@
 
 resource "azurerm_storage_blob" "uploadFilesHTML" {
-  for_each = fileset("${path.module}/Portfolio", "*.html")
+  for_each = fileset("${path.module}/Pipeline_Site", "*.html")
 
   name                   = "${each.value}"
   storage_account_name   = azurerm_storage_account.storageAccount.name
   storage_container_name = "$web"
   type                   = "Block"
-  source                 = "${path.module}/Portfolio/${each.value}"
+  source                 = "${path.module}/Pipeline_Site/${each.value}"
   content_type           = "text/html"
   depends_on = [azurerm_storage_account.storageAccount]
 }
 
 resource "azurerm_storage_blob" "uploadFilesCSS" {
-  for_each = fileset("${path.module}/Portfolio", "*.css")
+  for_each = fileset("${path.module}/Pipeline_Site", "*.css")
 
   name                   = "${each.value}"
   storage_account_name   = azurerm_storage_account.storageAccount.name
   storage_container_name = "$web"
   type                   = "Block"
-  source                 = "${path.module}/Portfolio/${each.value}"
+  source                 = "${path.module}/Pipeline_Site/${each.value}"
   content_type           = "text/css"
   depends_on = [azurerm_storage_account.storageAccount]
 }
 
 resource "azurerm_storage_blob" "uploadFilesJS" {
-  for_each = fileset("${path.module}/Portfolio", "*.js")
+  for_each = fileset("${path.module}/Pipeline_Site", "*.js")
 
   name                   = "${each.value}"
   storage_account_name   = azurerm_storage_account.storageAccount.name
   storage_container_name = "$web"
   type                   = "Block"
-  source                 = "${path.module}/Portfolio/${each.value}"
+  source                 = "${path.module}/Pipeline_Site/${each.value}"
   content_type           = "text/js"
   depends_on = [azurerm_storage_account.storageAccount]
 }
 
 resource "azurerm_storage_blob" "uploadFilesTTF" {
-  for_each = fileset("${path.module}/Portfolio", "*.ttf")
+  for_each = fileset("${path.module}/Pipeline_Site", "*.ttf")
 
   name                   = "${each.value}"
   storage_account_name   = azurerm_storage_account.storageAccount.name
   storage_container_name = "$web"
   type                   = "Block"
-  source                 = "${path.module}/Portfolio/${each.value}"
+  source                 = "${path.module}/Pipeline_Site/${each.value}"
   content_type           = "application/x-font-truetype"
   depends_on = [azurerm_storage_account.storageAccount]
 }
 
 resource "azurerm_storage_blob" "uploadFilesPNG" {
-  for_each = fileset("${path.module}/Portfolio", "*.png")
+  for_each = fileset("${path.module}/Pipeline_Site", "*.png")
 
   name                   = "${each.value}"
   storage_account_name   = azurerm_storage_account.storageAccount.name
   storage_container_name = "$web"
   type                   = "Block"
-  source                 = "${path.module}/Portfolio/${each.value}"
+  source                 = "${path.module}/Pipeline_Site/${each.value}"
   content_type           = "image/png"
   depends_on = [azurerm_storage_account.storageAccount]
 }
